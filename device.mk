0e3cc1822ce5e40422c9cebc723e97667ac4e6b9
goayandi <r.borutha@gmail.com>
2018-09-19 14:44:26 +0200

    Fix: Adding SELinux context for fixing logspam

--- device.mk
+++ device.mk
@@ -4,6 +4,7 @@ TARGET_PREBUILT_KERNEL := device/xiaomi/cappu/kernel
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/fstab.mt8173:root/fstab.mt8173 \
     $(LOCAL_PATH)/recovery/init.recovery.mt8173.rc:root/init.recovery.mt8173.rc \
+    $(LOCAL_PATH)/recovery/init.recovery.service.rc:root/init.recovery.service.rc \
     $(LOCAL_PATH)/recovery/ueventd.mt8173.rc:root/ueventd.mt8173.rc
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
